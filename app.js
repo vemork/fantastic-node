f2d3b2d80f5306eef4dc0b7d6aa3e57a5afd9cf3
Emmanuel.Rubio <Emmanuel.Rubio@vtex.com.br>
2021-06-09 20:54:51 -0500

    convirtiendo la multiplicación en async

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ console.clear();
 
 const base = 5;
 
-crearArchivoTabla(base);
+crearArchivoTabla(base)
+.then( nombreArchivo => console.log(`El archivo ${ nombreArchivo } fue creado`))
+.catch( err => console.log(`Se ha generado un error ${err}`) );
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,24 +1,30 @@
 const fs = require('fs');
 
 
-const crearArchivoTabla = ( base = 5) => {
-    console.log('==================================');
-    console.log(`Tabla de Multiplicar del ${base}`);
-    console.log('==================================');
-
-    let salida = '';
-
-    for (let index = 1; index <=10; index++) {
-        salida += `${ base } x ${ index } = ${ base * index }\n`;    
+const crearArchivoTabla = async( base = 5) => {
+   
+    try{
+        console.log('==================================');
+        console.log(`Tabla de Multiplicar del ${base}`);
+        console.log('==================================');
+    
+        let salida = '';
+    
+        for (let index = 1; index <=10; index++) {
+            salida += `${ base } x ${ index } = ${ base * index }\n`;    
+        }
+    
+        console.log(salida);
+    
+        fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+                if(err) throw err;
+            },
+         )
+    
+         return `tabla-${base}.txt`;
+    }catch(err){
+        throw err;
     }
-
-    console.log(salida);
-
-    fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-            if(err) throw err;
-            console.log(`tabla-${base}.txt creado`);
-        },
-     )
 };
 
 module.exports = {
